c015023a5afee6f929c5366f608ea9a88ef36fc1
Daniel <danieldossantosdacosta@gmail.com>
2022-11-22 17:29:52 -0300

    add folder for blog

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Blog de Daniel",
+  description: "Apenas aprendendo!",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Blog", link: "/articles/" },
+      { text: "External", link: "https://google.com" },
+    ],
+  },
+};
